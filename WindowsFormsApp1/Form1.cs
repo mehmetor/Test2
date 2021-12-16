6feb6e6db184b59e24d9331d4f26e997d8785c5b
mehmetor <mehmetakiforakci@gmail.com>
2021-12-16 12:19:47 +0300

    code changes !

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -13,5 +13,9 @@ namespace WindowsFormsApp1 {
         public Form1() {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) {
+            // ;)
+        }
     }
 }
--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -14,6 +14,7 @@ namespace WindowsFormsApp1 {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // OK
         }
     }
 }
